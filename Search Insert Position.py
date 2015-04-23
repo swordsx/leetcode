c8803aafcaa02aea7c67f234a88194e55196ab30
swordsx <shanxiang0325@msn.cn>
2015-04-23 22:12:18 +0800

    Create Search Insert Position.py

---
+++ Search Insert Position.py
@@ -0,0 +1,18 @@
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer}
+    def searchInsert(self, nums, target):
+        if not nums:
+            return 0
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) / 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
