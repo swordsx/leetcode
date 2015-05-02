94f38ae6c295fa0cf85bad2fe5a2ef493e3ebbe3
swordsx <shanxiang0325@msn.cn>
2015-05-02 16:14:11 +0800

    Update Symmetric Tree.py
    Solution2是遞歸版本。

--- Symmetric Tree.py
+++ Symmetric Tree.py
@@ -34,3 +34,25 @@ class Solution:
                     list.append(None)
             return list
         return BFSLeft(root) == BFSRight(root)
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution2:
+    # @param {TreeNode} root
+    # @return {boolean}
+    def isSymmetric(self, root):
+        def isSymmetric(self, left, right):
+            if not left and not right: return True
+            if (left and not right) or (right and not left): return False
+            if left.val != right.val: return False
+            return isSymmetric(self, left.left, right.right) and isSymmetric(self, left.right, right.left)
+
+        if not root: return True
+        return isSymmetric(self, root.left, root.right)
