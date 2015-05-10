d6dcfef8e381ac9370ebbf1a01e3021c9a95364f
swordsx <shanxiang0325@msn.cn>
2015-05-10 14:27:40 +0800

    Create Reverse Linked List.py
    叠代解法。

---
+++ Reverse Linked List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def reverseList(self, head):
+        cur = head
+        pre = None
+        while cur:
+            post = cur.next
+            cur.next = pre
+            pre = cur
+            cur = post
+        return pre
