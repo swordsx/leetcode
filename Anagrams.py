e40bf4361914a4e76f5ed6b99aa26c663c5ea359
swordsx <shanxiang0325@msn.cn>
2015-06-15 02:54:10 +0800

    Create Anagrams.py
    把字典转换成字符串再作为key，我也真是异想开天啊……

---
+++ Anagrams.py
@@ -0,0 +1,19 @@
+class Solution:
+    # @param {string[]} strs
+    # @return {string[]}
+    def anagrams(self, strs):
+        result = []
+        anagramsDict = collections.defaultdict(list)
+        for string in strs:
+            letterCntDict = collections.defaultdict(int)
+            for ch in string:
+                letterCntDict[ch] += 1
+            key = ''
+            for ch in 'abcdefghijklmnopqrstuvwxyz':
+                if ch in letterCntDict:
+                    key += ch + str(letterCntDict[ch])
+            anagramsDict[key].append(string)
+        for key in anagramsDict:
+            if len(anagramsDict[key]) > 1:
+                result += anagramsDict[key]
+        return result
