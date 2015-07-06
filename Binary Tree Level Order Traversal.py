5779ac8920d1359a5a9e02df8dae6a7e743b3849
swordsx <shanxiang0325@msn.cn>
2015-07-06 12:00:51 +0800

    Create Binary Tree Level Order Traversal.py

---
+++ Binary Tree Level Order Traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {integer[][]}
+    def levelOrder(self, root):
+        stack = [(root, 0)]
+        result = []
+        while stack:
+            cur, depth = stack.pop(0)
+            if cur:
+                if len(result) == depth: result.append([])
+                result[depth].append(cur.val)
+                stack.append((cur.left, depth + 1))
+                stack.append((cur.right, depth + 1))
+        return result
