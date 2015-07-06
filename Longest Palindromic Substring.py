71d4ec5bd0e0d58e268638b409142af843d86fd1
swordsx <shanxiang0325@msn.cn>
2015-07-06 18:45:05 +0800

    Update Longest Palindromic Substring.py
    終於理解了manacher算法，time cost reduces from 1700+ ms to 124ms

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -24,3 +24,25 @@ class Solution:
                 maxLen = curMaxLen
                 result = s[(i - offset + 1) : (i + offset + 1)]
         return result
+
+
+class Solution:
+    # @param {string} s
+    # @return {string}
+    def longestPalindrome(self, s):
+        ss = '$#' + '#'.join([ch for ch in s]) + '#%'
+        ID, mx, lenss = 1, 1, len(ss)
+        p = [1] * lenss
+        maxi = 0
+        
+        for i in range(1, lenss - 1):
+            if mx > i:
+                p[i] = min(p[ID * 2 - i], mx - i)
+            while ss[i + p[i]] == ss[i - p[i]]:               
+                p[i] += 1
+            if p[i] + i > mx:
+                ID, mx = i, p[i] + i
+            if p[i] > p[maxi]:
+                maxi = i
+        result = ss[maxi-p[maxi]+1:maxi+p[maxi]]
+        return ''.join(result.split('#'))
