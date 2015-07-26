08cfe21eaff670eab9a46c16ece0a9841dbf4cca
swordsx <shanxiang0325@msn.cn>
2015-07-26 16:34:03 +0800

    Update Min Stack.py
    400ms->100ms。
    原来push的是一个tuple，简直慢……

--- Min Stack.py
+++ Min Stack.py
@@ -2,23 +2,26 @@ class MinStack:
     # initialize your data structure here.
     def __init__(self):
         self.stack = []
+        self.minStack = []
 
     # @param x, an integer
     # @return an integer
     def push(self, x):
-        if self.stack:
-            self.stack.append((x, min(x, self.getMin())))
-        else: self.stack.append((x, x))
+        self.stack.append(x)
+        if not self.minStack or x <= self.minStack[-1]:
+            self.minStack.append(x)
         return x
 
     # @return nothing
     def pop(self):
-        self.stack.pop()
+        x = self.stack.pop()
+        if x == self.minStack[-1]:
+            self.minStack.pop()
 
     # @return an integer
     def top(self):
-        return self.stack[-1][0]
+        return self.stack[-1]
 
     # @return an integer
     def getMin(self):
-        return self.stack[-1][1]
+        return self.minStack[-1]
