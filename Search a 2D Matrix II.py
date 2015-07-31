b9673ff127e51365cf07741f42af5198736cc75a
swordsx <shanxiang0325@msn.cn>
2015-07-31 15:18:44 +0800

    Update Search a 2D Matrix II.py
    分治解法from COCI。不知何故竟然要用掉224ms

--- Search a 2D Matrix II.py
+++ Search a 2D Matrix II.py
@@ -14,3 +14,27 @@ class Solution:
                 elif matrix[i][mid] > target: high = mid - 1
                 else: low = mid + 1
         return False
+
+
+class Solution:
+    # @param {integer[][]} matrix
+    # @param {integer} target
+    # @return {boolean}
+    def searchMatrix(self, matrix, target):
+        def search(rowS, colS, rowE, colE):
+            if rowS == rowE and colS == colE:
+                return target == matrix[rowS][colS]
+            if rowS > rowE or colS > colE:
+                return False
+            if matrix[rowS][colS] > target or matrix[rowE][colE] < target:
+                return False
+            rowM = (rowS + rowE) / 2
+            colM = (colS + colE) / 2
+            return search(rowS, colS, rowM, colM) or \
+                   search(rowS, colM + 1, rowM, colE) or \
+                   search(rowM + 1, colS, rowE, colM) or \
+                   search(rowM + 1, colM + 1, rowE, colE)
+
+        if not matrix: return False
+        m, n = len(matrix), len(matrix[0])
+        return search(0, 0, m - 1, n - 1)
